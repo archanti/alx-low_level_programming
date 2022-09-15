b7bc84a79e34cafd310a24190fbe1530603772ce
archanti <intagemax@gmail.com>
2022-09-15 09:17:47 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,26 @@
-#include <stdio.h>
-/** This program prints _putchar
- *  * 
- *   * */
-int main() {
-	printf("_putchar \n");
-  	return 0;
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main -  prints Holberton, followed by a new line
+ *
+ *   * Return: Always 0 (Success)
+ *
+ *    */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return(0);
+
 }
