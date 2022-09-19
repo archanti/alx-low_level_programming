3f484557aa5230bc1a62d89906dcb3dc709ba7a5
archanti <intagemax@gmail.com>
2022-09-19 10:35:50 -0700

    Added puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,13 +6,11 @@
  */
 
 void _puts(char *str)
-
 {
+     	char *str;
 
-	int i;
+	str = "tHIS IS MY TEST STRING";
+      	_puts(str);
+	return (0);
 
-	for (i = 0; str[i] != '\0'; i++)
-		{	
-			_putchar(str[i]);
-			_putchar('\n');
 }
