6c8128bd744bb255b5ecbae60071fa6e3ce4cc1f
archanti <intagemax@gmail.com>
2022-09-26 23:35:28 -0700

    added strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,12 +2,12 @@
 
 
 /**
- * _strspn - gets the length of a prefix substring
- * @s: string to evaluate
- * @accept: string containing the list of characters to match in s
- * Return: the number of bytes in the initial segment
- * of s which consist only of bytes from accep
- */
+* _strspn - gets the length of a prefix substring
+* @s: string to evaluate
+* @accept: string containing the list of characters to match in s
+* Return: the number of bytes in the initial segment
+* of s which consist only of bytes from accep
+*/
 
 unsigned int _strspn(char *s, char *accept)
 
