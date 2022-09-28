42a50fe4001e7247ef9fafa8c197710b7b4b7bb9
archanti <intagemax@gmail.com>
2022-09-28 11:28:26 -0700

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
  * Return: length of the string
- * */
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
