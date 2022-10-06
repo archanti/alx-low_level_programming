28f5c468f95f55e3343da8f2606a4de1f4936aae
archanti <intagemax@gmail.com>
2022-10-05 17:06:18 -0700

    added 2 str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
-#include "mainn.h"
+#include "main.h"
 
 /**
  * _strdup - copies the string given as parameter
  * @str: string to duplicate
  * Return: pointer to the copied string (Success), NULL (Error)
- * */
+ */
 char *_strdup(char *str)
 {
 	char *dup;
